3225a44f4e83d33d5319a7b70f2c824749f364a9
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-19 17:44:13 -0600

    lint.sh: ensure script can only be run within Nix flake shell

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -24,6 +24,43 @@ change_to_project_root() {
   fi
 }
 
+in_nix_dev_shell() {
+  # The IN_NIX_SHELL environment variable is only present in Nix flake dev shells.
+  case "${IN_NIX_SHELL:-}" in
+    pure | impure) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
+get_script_path() {
+  git ls-files --full-name "${BASH_SOURCE[0]}"
+}
+
+print_nix_shell_error() {
+  local script_name="$1"
+
+  local message="${script_name} must be run inside a Nix flake development shell.
+
+To enter the flake shell, run:
+  $ nix develop
+  $ ./${script_name}
+
+Alternatively, you can run this script ad hoc without entering the shell:
+  $ nix develop .#adhoc --command ./${script_name}"
+
+  printf "%s\n" "$message" >&2
+}
+
+assert_in_nix_shell_or_exit() {
+  local script_name="$1"
+
+  in_nix_dev_shell && return 0
+
+  print_nix_shell_error "$script_name"
+
+  exit 1
+}
+
 run_shellcheck() {
   # Store all shell scripts and shell-based dotfiles in an array.
   mapfile -d '' files < <(find . -type f \( -name "*.sh" -o -name "*.bash" -o -name "dot_bash*" ! -name "*.tmpl" -o -name "dot_profile" \) -print0)
@@ -48,6 +85,7 @@ run_shellcheck() {
 
 main() {
   change_to_project_root "$(get_project_root)"
+  assert_in_nix_shell_or_exit "$(get_script_path)"
   run_shellcheck
 }
 
