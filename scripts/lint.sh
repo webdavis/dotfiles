073b272e3580561e5155d5c74954ab602c3d90e3
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-19 17:25:49 -0600

    lint.sh: wrap all script logic in `main` function

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,25 +3,52 @@
 # Exit immediately if any command fails (including compound commands).
 set -euo pipefail
 
-# Change to project root.
-cd "$(git rev-parse --show-toplevel)"
+get_project_root() {
+  # Change to project root.
+  git rev-parse --show-toplevel
+}
 
-# Store all shell scripts and shell-based dotfiles in an array.
-mapfile -d '' files < <(find . -type f \( -name "*.sh" -o -name "*.bash" -o -name "dot_bash*" ! -name "*.tmpl" -o -name "dot_profile" \) -print0)
+change_to_project_root() {
+  # Ensure this script runs from the project root.
 
-if [[ ${#files[@]} -eq 0 ]]; then
-  echo "No shell scripts or shell-based dotfiles found."
-  exit 0
-fi
+  local project_root="$1"
 
-echo "✅ Found ${#files[@]} file(s) to lint:"
-for f in "${files[@]}"; do
-  echo "$f"
-done
+  if [[ -z ${project_root:-} ]]; then
+    echo "Error: could not determine project root directory (are you in a Git repository?)" >&2
+    exit 1
+  fi
 
-# Run shellcheck on all files.
-for file in "${files[@]}"; do
-  echo "Linting: $file"
+  if ! cd "$project_root"; then
+    echo "Error: could not change into project root directory (${project_root##*/})" >&2
+    exit 1
+  fi
+}
 
-  shellcheck "$file"
-done
+run_shellcheck() {
+  # Store all shell scripts and shell-based dotfiles in an array.
+  mapfile -d '' files < <(find . -type f \( -name "*.sh" -o -name "*.bash" -o -name "dot_bash*" ! -name "*.tmpl" -o -name "dot_profile" \) -print0)
+
+  if [[ ${#files[@]} -eq 0 ]]; then
+    echo "No shell scripts or shell-based dotfiles found."
+    exit 0
+  fi
+
+  echo "✅ Found ${#files[@]} file(s) to lint:"
+    for f in "${files[@]}"; do
+      echo "$f"
+    done
+
+  # Run shellcheck on all files.
+  for file in "${files[@]}"; do
+    echo "Linting: $file"
+
+    shellcheck "$file"
+  done
+}
+
+main() {
+  change_to_project_root "$(get_project_root)"
+  run_shellcheck
+}
+
+main "$@"
