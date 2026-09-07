d7cc2ff2878b2ec7712c43fc1bb5c1da7dbb18a4
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2026-09-07 17:46:13 -0600

    fix(nvim): avoid duplicate herdr sidebar keymaps

--- dot_config/nvim/lua/plugins/herdr-nvim.lua
+++ dot_config/nvim/lua/plugins/herdr-nvim.lua
@@ -1,9 +1,44 @@
--- The nvim half of ChmaraX/herdr-nvim (the herdr plugin is installed by the
--- dotfiles' run_after_53): line-anchored annotations sent back to agents.
+-- Neovim and the sidebar launcher both call setup. Keep mapping ownership here.
 return {
   "ChmaraX/herdr-nvim",
   lazy = false,
-  opts = {
-    prefix = "<leader>A", -- the default <leader>a collides with other maps here
+  opts = { prefix = "<leader>A", keymaps = false },
+  keys = {
+    {
+      "<leader>Ac",
+      function()
+        require("herdr-nvim").comment_selection()
+      end,
+      mode = "x",
+      desc = "herdr-nvim: comment selection",
+    },
+    {
+      "<leader>Ac",
+      function()
+        require("herdr-nvim").comment_line()
+      end,
+      desc = "herdr-nvim: comment line",
+    },
+    {
+      "<leader>Al",
+      function()
+        require("herdr-nvim").list_comments()
+      end,
+      desc = "herdr-nvim: list comments",
+    },
+    {
+      "<leader>As",
+      function()
+        require("herdr-nvim").send_all({ submit = false })
+      end,
+      desc = "herdr-nvim: paste comments to agent",
+    },
+    {
+      "<leader>AS",
+      function()
+        require("herdr-nvim").send_all({ submit = true })
+      end,
+      desc = "herdr-nvim: send comments to agent",
+    },
   },
 }
